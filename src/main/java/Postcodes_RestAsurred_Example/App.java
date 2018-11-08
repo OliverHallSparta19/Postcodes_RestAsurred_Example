7f2fa857a0a7e7843c18890c858940ed8343244b
Oliver <OHall@spartaglobal.com>
2018-11-08 09:47:04 +0000

    first commit

---
+++ src/main/java/Postcodes_RestAsurred_Example/App.java
@@ -0,0 +1,13 @@
+package Postcodes_RestAsurred_Example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
